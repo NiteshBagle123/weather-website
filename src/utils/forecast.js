ee8d6e2dec0cd752f60dcc89afa9c5a383f2533e
Bagle <baglen@pun-baglen-m.ds.ad.adp.com>
2019-12-24 15:35:53 +0530

    more information display on forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -7,8 +7,8 @@ const forecast = (lat, long, callback) => {
         } else if(body.error){
             callback('Unable to find location', undefined);
         } else {
-            const { temperature, precipProbability } = body.currently;
-            callback(undefined, `${body.daily.data[0].summary} It is currently ${temperature} out. There is ${precipProbability}% chance of rain`);
+            const { temperature, precipProbability, pressure, windSpeed } = body.currently;
+            callback(undefined, `${body.daily.data[0].summary} It is currently ${temperature} out. There is ${precipProbability}% chance of rain. Pressure is ${pressure} and wind speed is ${windSpeed}`);
         }
     });
 }
